22473bec2441c01e310fc5880bc353dcf53cf82a
lewis941001 <lewis1004@naver.com>
2021-06-27 15:25:41 +0900

    [210627_clone]fix

--- main.c
+++ main.c
@@ -1,43 +1,111 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
+#include "main.h"
 
+//////////////////////////////////////parent
 
-int redirect_in(const char *file)
+void connect_stdout_pipe_w(int pipefd[2])
+{
+	dup2(STDOUT_FILENO, pipefd[WRITE]);
+	close(pipefd[READ]);
+	close(pipefd[WRITE]);
+}
+
+int connect_file_to_stdin(const char *infile)
 {
 	int fd;
 
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
+	fd = open(infile, O_RDWR);
+	if(fd < 0)
 	{
-		perror(file);
-		return (-1);
+		perror(infile);
+		return (ERROR);
 	}
-	dup2(fd, STDIN_FILENO);
+	dup2(fd ,STDIN_FILENO);
 	close(fd);
-	return (0);
+	return (SUCCESS);
 }
 
-int main (int argc, char const *argv[])
+void init_exe(t_exe *exe, char *argv)
 {
-	pid_t pid;
-	int i = 5;
-	pid = fork();
+	char **cmd;
+	
+	cmd = ft_split(cmd, ' ');
+	
+	exe->path[0] = ft_strjoin("/usr/local/bin/", cmd[0]);
+	exe->path[1] = ft_strjoin("/usr/bin/", cmd[0]);
+	exe->path[2] = ft_strjoin("/bin/", cmd[0]);
+	exe->path[3] = ft_strjoin("/usr/sbin/", cmd[0]);
+	exe->path[4] = ft_strjoin("/sbin/", cmd[0]);
 
+	exe->argv = cmd;
+	exe->envp = NULL;
+}
+
+void use_pipe_w_to_stdout(char *argv)
+{
+	t_exe exe;
+	int i;
+
+	i = 0;
+	init_exe(&exe, argv);
+	while(i < 5)
+		execve(exe.path[i], exe.argv, exe.envp);
+	perror(argv);
+}
+////////////////////////////////////////
+
+int connect_file_to_stdout(char *outfile)
+{
+	int fd;
+
+	fd = open(outfile, O_RDWR | O_CREAT, 0644);
+	if( fd < 0)
+	{
+		perror(outfile);
+		return(ERROR);
+	}
+	dup2(fd ,STDOUT_FILENO);
+	close(fd);
+	return (SUCCESS);
+}
+
+void connect_stdin_pipe_r(int pipefd[2])
+{
+	dup2(pipefd[1], STDIN_FILENO);
+	close(pipefd[0]);
+	close(pipefd[1]);
+}
+
+void use_pipe_r_to_file(char *argv)
+{
+	t_exe exe;
+	int i;
+
+	i = 0;
+	init_exe(&exe, argv);
+	while(i < 5)
+		execve(exe.path[i], exe.argv, exe.envp);
+	perror(argv);
+}
+
+int main(int argc, char *argv[])
+{
+	int pipefd[2];
+	pid_t pid;
+
+
+	pipe(pipefd);
+	pid = fork();
 	if(pid > 0)
 	{
-		printf("%d\n", ++i);
-		printf("parent pid : %d\n", getpid());
-		printf("pid from fork() to the parent %d\n", pid);
-		redirect_in(argv[1]);	//[i]
-		execve("/bin/cat", STDIN_FILENO, 0);
+		connect_file_to_stdin(argv[1]);
+		connect_stdout_pipe_w(pipefd);
+		use_pipe_w_to_stdout(argv[2]);
 	}
 	else if(pid == 0)
 	{
-		printf("%d\n", i);
-		printf("child PID : %d\n" ,getpid());
-		printf("pid from fork() ro the child: %d\n", pid);
+		connect_file_to_stdout(argv[4]);			
+		connect_stdin_pipe_r(pipefd);
+		use_pipe_r_to_file(argv[3]);	
 	}
-	return (0);
+	return(0);
 }
-
