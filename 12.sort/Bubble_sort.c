a337991dff9bca461b8b8b1942a1d0fb28a7ab79
lewis941001 <lewis1004@naver.com>
2021-07-28 21:20:09 +0900

    [210728_add]Bubble_sort

---
+++ 12.sort/Bubble_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX_SIZE 10
+#define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t) )
+
+int list[MAX_SIZE];
+int n;
+
+void bubble_sort(int list[], int n)
+{
+	int i, j, temp;
+	for (i = n - 1; i > 0; i--)
+	{
+		for(j = 0; j < i; j++)
+			if(list[j] > list[j + 1])
+				SWAP(list[j], list[j + 1], temp);
+	}
+}
+
+int main()
+{
+	int i;
+	n = MAX_SIZE;
+	srand(time(NULL));
+	for(i = 0; i < n; i++)
+		list[i] = rand() % 100;
+	
+	printf("before_sort\n");
+	for(i = 0; i < n; i++)
+		printf("%d ", list[i]);
+	printf("\n");
+	
+	printf("after_sort\n");
+	bubble_sort(list, n);
+	for(i = 0; i < n; i++)
+		printf("%d ", list[i]);
+	printf("\n");
+	return 0;
+}
